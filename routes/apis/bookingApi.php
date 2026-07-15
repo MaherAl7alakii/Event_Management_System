45bd38049116db8574200ac5c1e32e4fb31682c6
MaherAl7alaki <maherhalaky@gmail.com>
2026-07-15 10:10:14 +0300

    feat(Middleware): implement provider approval verification middleware

--- routes/apis/bookingApi.php
+++ routes/apis/bookingApi.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\BookingController;
 use App\Http\Controllers\BookingStatusController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('bookings')->middleware(['auth:api','verified.email','setLanguage'])->group(function () {
+Route::prefix('bookings')->middleware(['auth:api','verified.email','setLanguage','provider.approved'])->group(function () {
 
     Route::get('/', [BookingController::class, 'index']);
 
