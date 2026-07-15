f6a88792d457019df8ce8098453f5146c426321f
MaherAl7alaki <maherhalaky@gmail.com>
2026-07-15 10:01:48 +0300

    feat(booking): add controllers and routes for booking operations

---
+++ app/Http/Controllers/BookingStatusController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\Booking\BookingShowResource;
+use App\Models\Booking;
+use App\Services\BookingService;
+use App\Traits\ResponseTrait;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Symfony\Component\HttpFoundation\Response;
+
+class BookingStatusController extends Controller
+{
+    use ResponseTrait;
+    use AuthorizesRequests;
+
+    protected BookingService $bookingService;
+    protected string $resourceName = 'messages.resources.booking';
+
+    public function __construct(BookingService $bookingService)
+    {
+        $this->bookingService = $bookingService;
+    }
+
+
+    public function accept(Booking $booking)
+    {
+
+        $this->authorize('accept', $booking);
+
+
+        $updatedBooking = $this->bookingService->respondToBooking($booking, 'accept');
+
+        return $this->apiResponse(
+            new BookingShowResource($updatedBooking),
+            __('messages.booking_accepted_success', ['resource' => __($this->resourceName)]),
+            Response::HTTP_OK
+        );
+    }
+
+
+    public function reject(Booking $booking)
+    {
+        $this->authorize('reject', $booking);
+
+        $updatedBooking = $this->bookingService->respondToBooking($booking, 'reject');
+
+        return $this->apiResponse(
+            new BookingShowResource($updatedBooking),
+            __('messages.booking_rejected_success', ['resource' => __($this->resourceName)]),
+            Response::HTTP_OK
+        );
+    }
+}
